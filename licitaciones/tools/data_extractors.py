01a971e700d14c04960b914cc3f1c7b2d3da47c5
Daniel Moreno <danielmoreno@Daniels-MacBook-Air.local>
2024-04-28 15:36:14 +0200

    SIIIIIIIIIIIIII EXTRAE DATOS Y ESTA SISTEMATIZADO. Ajudicadora lo extrae mal.

--- licitaciones/tools/data_extractors.py
+++ licitaciones/tools/data_extractors.py
@@ -10,10 +10,25 @@ class FinancialData(BaseModel):
     amount: float = Field(..., title="Amount", description="The total amount of the contract")
     currency: str = Field(..., title="Currency", description="The currency of the contract amount")
 
+class AdjudicadoraData(BaseModel):
+    adjudicadora: str = Field(..., title="Adjudicadora", description="The name of the Contracting Authority")
+    expediente: str = Field(None, title="Expediente", description="The contract number")
+
+class TipoData(BaseModel):
+    tipo: str = Field(..., title="Tipo", description="The type of contract: Contrato de obras, Contrato de concesion de obras, Contrato de concesion Servicios, Contrato de suministro, Contrato de Servicios, Contrato Mixto, Contrato menor")
+
+class TramitacionData(BaseModel):
+    tramitacion: str = Field(..., title="Tramitacion", description="The type of procedure:  Ordinaria, Urgente, Emergencia")
+    procedimiento: str = Field(None, title="Procedimiento", description="The procedure type: Abierto, Restringido, Negociado con publicidad, Negociado sin publicidad, Diálogo competitivo, Asociacion para la innovacion, Simplificado, Basado en un Acuerdo Marco, Sistema Dinamico de Adquisicion.")
+
 # Define the DataExtractor class
 class DataExtractor:
-    def __init__(self, api_key: str ='ollama', base_url="http://localhost:11434/v1", text: str = None):
-        self.text = text
+    def __init__(self, api_key: str ='ollama', base_url="http://localhost:11434/v1", text_company: str = None, text_amount: str = None, text_adjudicadora: str = None, text_tipo: str = None, text_tramitacion: str = None):
+        self.text_company = text_company
+        self.text_amount = text_amount
+        self.text_adjudicadora = text_adjudicadora
+        self.text_tipo = text_tipo
+        self.text_tramitacion = text_tramitacion
         self.client = instructor.patch(
             OpenAI(
                 base_url=base_url,
@@ -26,11 +41,26 @@ class DataExtractor:
         self.company_name = None
         self.amount = None
         self.currency = None
+        self.adjudicadora = None
+        self.tipo = None
+        self.tramitacion = None
+        self.procedimiento = None
+
+        if self.text_company is not None:
+            self.extract_company()
+        if self.text_amount is not None:
+            self.extract_amount()
+        if self.text_adjudicadora is not None:
+            self.extract_adjudicadora()
+        if self.text_tipo is not None:
+            self.extract_tipo()
+        if self.text_tramitacion is not None:
+            self.extract_tramitacion()
 
     def extract_company(self):
         content = f"""
         Extract the name of the company from the following text:
-        {self.text}
+        {self.text_company}
         """
         response = self.client.chat.completions.create(
             model="llama3",
@@ -43,8 +73,8 @@ class DataExtractor:
 
     def extract_amount(self):
         content = f"""
-        Extract the total contract amount and its currency from the following text:
-        {self.text}
+        Extract the total contract amount and its currency from the following text, normally it is referred to as "Valor estimado del contrato":
+        {self.text_amount}
         """
         response = self.client.chat.completions.create(
             model="llama3",
@@ -54,3 +84,61 @@ class DataExtractor:
         )
         self.amount = response.amount
         self.currency = response.currency
+
+    def extract_adjudicadora(self):
+        content = f"""
+        Extract the name of the Contracting Authority from the text. Normally it is referred to as "Organismo" or "Entidad adjudicadora". Extract also "Numero de expediente" if available. IT IS NOT A PERSON, IT IS AN INSTITUTION OF PUBLIC ADMINISTRATION. Example text:
+        {self.text_adjudicadora}
+        """
+        response = self.client.chat.completions.create(
+            model="llama3",
+            messages=[{"role": "user", "content": content}],
+            response_model=AdjudicadoraData,
+            max_retries=10
+        )
+        self.adjudicadora = response.adjudicadora
+        self.expediente = response.expediente
+
+    def extract_tipo(self):
+        content = f"""
+        Extract the type of contract from the text. Normally it is referred to as "Objetivo de Contrato Tipo".
+        The different types of contracts are: The type of contract: Contrato de obras, Contrato de concesion de obras, Contrato de concesion Servicios, Contrato de suministro, Contrato de Servicios, Contrato Mixto, Contrato menor
+        Example text:
+        {self.text_tipo}
+        """
+        response = self.client.chat.completions.create(
+            model="llama3",
+            messages=[{"role": "user", "content": content}],
+            response_model=TipoData,
+            max_retries=10
+        )
+        self.tipo = response.tipo
+    
+    def extract_tramitacion(self):
+        content = f"""
+        Extract the "tramitacion" and "procedimiento" from the text.
+        The types of "tramitacion" are: "Ordinaria", "Urgente", "Emergencia".
+        The types of procedimiento are: "Abierto", "Restringido", "Negociado con publicidad", "Negociado sin publicidad", "Diálogo competitivo", "Asociacion para la innovacion", "Simplificado", "Basado en un Acuerdo Marco", "Sistema Dinamico de Adquisicion".
+        Example text:
+        {self.text_tramitacion}
+        """
+        response = self.client.chat.completions.create(
+            model="llama3",
+            messages=[{"role": "user", "content": content}],
+            response_model=TramitacionData,
+            max_retries=10
+        )
+        self.tramitacion = response.tramitacion
+        self.procedimiento = response.procedimiento
+
+    def __str__(self) -> str:
+        printed_text = f"""
+        Company Name: {self.company_name}
+        Amount: {self.amount} in  {self.currency}
+        Adjudicadora: {self.adjudicadora}
+        Expediente: {self.expediente}
+        Tipo: {self.tipo}
+        Tramitacion: {self.tramitacion}
+        Procedimiento: {self.procedimiento}
+        """
+        return printed_text
--- licitaciones/tools/similarity_search.py
+++ licitaciones/tools/similarity_search.py
@@ -13,16 +13,21 @@ class EmbeddingsDB:
                     metadata={"hnsw:space": "cosine"}
                 )
             except:
-                print(f"Collection {self.pdf_reader.filename} already exists. Do you want to overwrite it?")
-                overwrite = input("Enter 'y' to overwrite, any other key to cancel: ")
-                if overwrite.lower() == 'y':
-                    self.collection = self.client.delete_collection(name=self.pdf_reader.filename)
-                    self.collection = self.client.create_collection(
-                        name=self.pdf_reader.filename, 
-                        metadata={"hnsw:space": "cosine"}
-                    )
-                else:
-                    self.collection = self.collection = self.client.get_collection(name=self.pdf_reader.filename)
+                # print(f"Collection {self.pdf_reader.filename} already exists. Do you want to overwrite it?")
+                # overwrite = input("Enter 'y' to overwrite, any other key to cancel: ")
+                # if overwrite.lower() == 'y':
+                #     self.client.delete_collection(name=self.pdf_reader.filename)
+                #     self.collection = self.client.create_collection(
+                #         name=self.pdf_reader.filename, 
+                #         metadata={"hnsw:space": "cosine"}
+                #     )
+                # else:
+                #     self.collection = self.collection = self.client.get_collection(name=self.pdf_reader.filename)
+                self.client.delete_collection(name=self.pdf_reader.filename)
+                self.collection = self.client.create_collection(
+                    name=self.pdf_reader.filename, 
+                    metadata={"hnsw:space": "cosine"}
+                )
     def get_embedding(self, text):
         """Generate an embedding for the provided text using the 'llama3' model."""
         embedding_dict = ollama.embeddings(model='llama3', prompt=text)
@@ -51,3 +56,42 @@ class EmbeddingsDB:
             n_results=n_results
         )
         return results
+
+class SimilaritySearch(EmbeddingsDB):
+    def __init__(self, pdf_reader=None):
+        super().__init__(pdf_reader=pdf_reader)
+        if pdf_reader:
+            self.add_documents_to_db()
+
+    def similar_text(self, query_text, n_results=2):
+        """Search for documents similar to the given text. Then get the ids of the documents and return the text using pdf_reader.get_part(id)."""
+        results = self.query_documents(query_text, n_results=n_results)
+
+        print(results)
+        # Get ids of the documents
+        ids = [int(id) for id in results['ids'][0]]
+
+        # Get the documents from the PDF reader
+        text = ""
+        for id in ids:
+            text += self.pdf_reader.get_part(id).text + "\n"
+
+        # Get the number of page
+        pages = set([self.pdf_reader.get_part(id).metadata['page'] for id in ids])
+        return text, pages
+    
+class Search(SimilaritySearch):
+    def __init__(self, pdf_reader=None):
+        super().__init__(pdf_reader=pdf_reader)
+        self.query_company = "Company which won the contract"
+        self.query_amount = 'total contract amount "Valor estimado del contrato"'
+        self.query_adjudicadora = 'Contracting Authority referred as "Organismo" or "Entidad adjudicadora"' # I do not write name because it is closer to the name of a person, for example Isabel, than to the name of an institution
+        self.query_tipo = 'type of contract "Objetivo de contrato", "Tipo"'
+        self.query_tramitacion = 'type of procedure "Procedimiento", "Tramitación"'
+
+
+        self.text_company = self.similar_text(self.query_company, n_results=2)
+        self.text_amount = self.similar_text(self.query_amount, n_results=2)
+        self.text_adjudicadora = self.similar_text(self.query_adjudicadora, n_results=2)
+        self.text_tipo = self.similar_text(self.query_tipo, n_results=2)
+        self.text_tramitacion = self.similar_text(self.query_tramitacion, n_results=2)
