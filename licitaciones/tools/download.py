9a2d7dff46d22b565faec478a58fc22c2671ab56
Daniel Moreno <danielmoreno@Daniels-MacBook-Air.local>
2024-05-13 00:15:58 +0200

    procesamiento grande. voy a tirar por hacer solo un ano por ahora

--- licitaciones/tools/download.py
+++ licitaciones/tools/download.py
@@ -1,29 +1,36 @@
 import requests
+import os
 
-def download_pdf(url: str = "/boe/dias/2012/03/08/pdfs/BOE-B-2012-7789.pdf"):
+def download_pdf(url: str):
+    base_url = "https://boe.es"
+    full_url = base_url + url 
+    filename = full_url.split("/")[-1]
+    path = f"pdfs/{filename}"
 
-    url = "https://boe.es" + url 
-    filename = url.split("/")[-1]
+    if not os.path.exists('pdfs'):
+        os.makedirs('pdfs')
 
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        path = "pdfs/" + filename
-        with open(path, 'wb') as f:
-            f.write(response.content)
-        print(f"PDF {filename} downloaded successfully.")
+    if os.path.exists(path):
+        print(f"PDF {filename} already downloaded.")
     else:
-        print("Failed to download PDF. Status code:", response.status_code)
+        response = requests.get(full_url)
+        if response.status_code == 200:
+            with open(path, 'wb') as f:
+                f.write(response.content)
+            print(f"PDF {filename} downloaded successfully.")
+        else:
+            print("Failed to download PDF. Status code:", response.status_code)
+
 
 # Download all pdfs from resoluciones_50_embeddings.json
 
 import json
 
 def download_all_resoluciones():
-    with open("resoluciones_50_embeddings.json", "r") as file:
+    with open("licitaciones.json", "r") as file:
         resoluciones = json.load(file)
 
     for _, items in resoluciones.items():
         for item in items:
-            url = item[3]
+            url = item[2]
             download_pdf(url)
